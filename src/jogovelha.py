f97888eff317aab10947909b708555ef4790f4bd
mcde44 <42256586+mcde44@users.noreply.github.com>
2018-08-23 21:59:51 -0300

    Corrigindo os Erros

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -3,7 +3,7 @@ def inicializar():
     for i in range(3):
         linha = []
         for j in range(3):
-            linha.append("x")
+            linha.append(".")
         tab.append(linha)
     return tab
 
@@ -13,4 +13,4 @@ def main():
 
 
 if __name__ == "__main__":
-main()
+    main()
